dae1697fc7066a63d2e32a9007534ed1c5e8d413
abidibo <abidibo@gmail.com>
2019-07-10 14:24:01 +0200

    bumps version number to 0.2.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-subject-imagefield',
-    version='0.2.0',
+    version='0.2.1',
     packages=['subject_imagefield'],
     include_package_data=True,
     license='MIT License',
